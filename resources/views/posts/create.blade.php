a11f13f015cc78b0b42b327f8bd31db120981ec6
hide-the-6st <coffee.and.music0423+programing@gmail.com>
2020-02-26 22:25:02 +0900

    change file & add route

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,11 +10,17 @@
   @endif
   <div class="card">
     <div class="card-body">
-      <form>
+      <form action="{{ route('posts.store') }}" method="POST">
+        @csrf
         <div class="form-group">
-          <label for="exampleInputEmail1">title</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-          placeholder="Enter email" name="title">
+          <label for="exampleInputTitle">title:</label>
+          <input type="text" class="form-control" id="exampleInputTitle" aria-describedby="titleHelp"
+          placeholder="タイトルを入力してください(必須ではありません)" name="title">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputText">comment:</label>
+          <textarea class="form-control" id="exampleInputComment" aria-describedby="commentHelp"
+          placeholder="本文を入力してください" name="content"></textarea>
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
